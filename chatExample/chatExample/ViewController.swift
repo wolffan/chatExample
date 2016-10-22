80c40d74fd6f24b32b96582444dec82f41f09faf
Raimon Lapuente <r.lapuente@gmail.com>
2016-10-22 16:25:27 +0200

    Added screen elements and autolayout for initial view controller

--- chatExample/chatExample/ViewController.swift
+++ chatExample/chatExample/ViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var messageImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = .red
+        
+        self.loginButton.layer.cornerRadius = 5.0
+        self.usernameField.layer.cornerRadius = 5.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginButtonPressed(_ sender: AnyObject) {
     }
-
 
 }
 
